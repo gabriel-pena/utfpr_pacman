e22b06f8f9e2e505b8743298a40dc1b3ea094a72
Cesar Mauricio Chauchuty <chauchuty@alunos.utfpr.edu.br>
2018-11-08 16:29:12 -0200

    Update pacman_v1.0.c
    fantasma[2].ia = 4
    fantasma[3].ia = 5

--- pacman_v1.0.c
+++ pacman_v1.0.c
@@ -486,8 +486,8 @@ void gamePlay(){
 	player.simbolo = 67, player.pos_i = 1, player.pos_j = 2, player.nav = 0, player.pontos = 0, player.m_pontos = 0;
 	fantasma[0].simbolo = 35,fantasma[0].pos_i = 6, fantasma[0].pos_j = 9, fantasma[0].nav = 0, fantasma[0].ia = 4;
 	fantasma[1].simbolo = 36,fantasma[1].pos_i = 6, fantasma[1].pos_j = 10, fantasma[1].nav = 1, fantasma[1].ia = 4;
-	fantasma[2].simbolo = 37,fantasma[2].pos_i = 7, fantasma[2].pos_j = 9, fantasma[2].nav = 2, fantasma[2].ia = 5;
-	fantasma[3].simbolo = 38,fantasma[3].pos_i = 7, fantasma[3].pos_j = 10, fantasma[3].nav = 3, fantasma[3].ia = 6;
+	fantasma[2].simbolo = 37,fantasma[2].pos_i = 7, fantasma[2].pos_j = 9, fantasma[2].nav = 2, fantasma[2].ia = 4;
+	fantasma[3].simbolo = 38,fantasma[3].pos_i = 7, fantasma[3].pos_j = 10, fantasma[3].nav = 3, fantasma[3].ia = 5;
 	
 	loading();
 	
